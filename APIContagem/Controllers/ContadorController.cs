52b5304864715ccbd3d2abe61fd94de761077f8d
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-11 02:13:57 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste AKS",
+                    Local = "Teste AKS - Janeiro-2020",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
