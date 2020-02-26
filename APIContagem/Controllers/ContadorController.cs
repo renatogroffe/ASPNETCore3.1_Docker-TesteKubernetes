399ef030878723057d8d7b07e91a9f43f2b60e8a
Renato Groffe <renato.groffe@yahoo.com.br>
2020-02-26 19:40:19 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Azure DevOps + Kubernetes + B2W",
+                    Local = "Azure DevOps",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
